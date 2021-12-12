e4739295dbefb1d92224d8c26ec6fa4517f4e6a6
pouiee <73872467+pouiee@users.noreply.github.com>
2021-12-12 10:45:45 -0800

    Organism v0.3
    An update to the Organism file, containing the base code for my evolution chamber thus far.
    Changes to this version include:
    - removal of various superfluous get functions in the three main classes
    - attrition and therefore starvation of organisms
    - addition of age to list of features for food and organisms, to be implemented in full effect for food in the next iteration
    - fleshing out of the move() function for organism and proper implementation of their interactions with other organisms and food with regards to consuming food and each other
    - full overhaul of the Food class
    - visual implementation of environment fully functional
    - other small QOL changes

--- Organism.py
+++ Organism.py
@@ -4,19 +4,33 @@ org_compendium = []
 food_compendium = []
 
 
-def pass_time(land, turns):
-    for org in org_compendium:
-        org.move(land, random.randint(-1, 1))
-        reproduce(land)
-    if turns == 0:
-        return
-    land.get_env()
-    pass_time(land, turns - 1)
+def status_report():
+    print('organisms alive: ' + str(len(org_compendium)))
+    print('food available: ' + str(len(food_compendium)))
+
+
+def pass_time(env, turns, elapsed_time=0):
+    while turns > 0:
+        env.food_grow()
+        for org in org_compendium:
+            org.move(env)
+            if org.alive:
+                org.attrition(env)
+                org.set_age(1)
+        reproduce(env)
+        env.print_env()
+        turns -= 1
+        elapsed_time += 1
+        if len(org_compendium) <= 0:
+            print('death has overcome life at turn ' + str(elapsed_time))
+            return
+        print('********** ' + str(elapsed_time) + ' turns **********')
+    env.print_env()
 
 
 def output_org_list():
     for org in org_compendium:
-        org.to_string()
+        print(org.features)
 
 
 def output_food_list():
@@ -24,6 +38,7 @@ def output_food_list():
         food.to_string()
 
 
+# genome/ reproduction functions
 def create_first_genome():
     genome = []
     for i in range(4):
@@ -31,7 +46,9 @@ def create_first_genome():
     return genome
 
 
-def interpret_genome(genome=create_first_genome()):
+def interpret_genome(genome=None):
+    if genome is None:
+        genome = create_first_genome()
     traits = {}
     behavior_dict = {
         'AB': 'Hostile',
@@ -45,7 +62,7 @@ def interpret_genome(genome=create_first_genome()):
     }
     agility_dict = {
         'AB': 'Fast',
-        'BA': 'Small',
+        'BA': 'Slow',
         'BB': 'Not Viable'
     }
     mutation_dict = {
@@ -74,105 +91,119 @@ def mutate(genome):
                 mutated_genome.append('AB')
         else:
             mutated_genome.append(genome[gene])
-
     return genome
 
 
-def inherit(p_1, p_2):
+def inherit(mom_1, mom_2):
     new_genome = []
-    for gene in range(len(p_1.get_genome())):
+    for i in range(len(mom_1.genome)):
         if random.randint(0, 10) > 5:
-            new_genome.append(p_1.get_genome()[gene])
+            new_genome.insert(i, mom_1.genome[i])
         else:
-            new_genome.append(p_2.get_genome()[gene])
+            new_genome.insert(i, mom_2.genome[i])
     return mutate(new_genome)
 
 
-def reproduce(land):
-    for x in land.get_display():
-        if len(x) == 2:
-            Organism(x,
-                     inherit(find_org(x[0]), find_org(x[1])))
-        elif len(x) % 2 == 0:
-            for i in range(len(x)):
-                if i % 2 == 0:
-                    continue
-                Organism(2, inherit(find_org(i), find_org(x[i + 1])))
-        elif len(x) > 2:
-            for i in range(len(x)):
-                if i % 2 != 0:
-                    continue
-                Organism(2, inherit(find_org(x[0]), find_org(x[1])))
+# interaction with other organisms (consumption and reproduction)
+def reproduce(env):
+    temp_space = []
+    for space in env.display:
+        temp_space = [obj for obj in space if obj is Organism]
+    if len(temp_space) >= 2:
+        for i in range(len(temp_space)):
+            if (i + 1) % 2 == 0:
+                print(str(find_org(temp_space[i])) + ' and '
+                      + str(find_org(temp_space[i - 1])) + ' are attempting reproduction')
+                Organism(env, find_org(temp_space[i]).position,
+                         inherit(find_org(temp_space[i]), find_org(temp_space[i - 1])))
 
 
 def hunt(predator, prey):
-    prey_value = prey.get_health()
-    while predator.is_alive() and prey.is_alive():
+    prey_value = prey.features['Health']
+    while predator.alive and prey.alive:
         predator.hit(prey)
-        if prey.is_alive():
+        if prey.alive:
             prey.hit(predator)
-    if predator.is_alive():
+    if predator.alive:
         predator.set_health(prey_value)
 
 
-def consume(org, food):
-    if not org.is_hostile() and food in food_compendium:
-        org.set_health(food.get_value())
-        food.consumed()
-    elif org.is_hostile() and not food.is_hostile():
+def consume(env, org, food):
+    if food in food_compendium:
+        print(str(org.id) + ' foraged')
+        if not org.features['Hostile']:
+            org.set_health(food.value)
+        else:
+            org.set_health(food.value / 2)
+        env.remove_food(food)
+    elif org.features['Hostile'] and food in org_compendium and org != food:
         hunt(org, food)
+        if not food.alive:
+            env.remove_org(food)
 
 
+# movement helper functions
 def is_occupied(check_pos):
-    for org in org_compendium:
-        if check_pos == org.get_position():
-            return True
-    for food in food_compendium:
-        if check_pos == food.get_position():
-            return True
+    if get_at_pos(check_pos):
+        return True
     return False
 
 
 def find_org(org_id):
-    for org in org_compendium:
-        if org_id == org.get_id():
-            return org
+    if org_id is int:
+        for org in org_compendium:
+            if org_id == org.id:
+                return org
+    elif org_id is str:
+        for food in food_compendium:
+            if org_id == food.id:
+                return food
 
 
 def get_at_pos(position):
     for org in org_compendium:
-        if position == org.get_position():
+        if position == org.position:
             return org
     for food in food_compendium:
-        if position == food.get_position():
+        if position == food.position:
             return food
     return None
 
 
-def update_env(env, org_1, destination):
-    env.add_obj(org_1, destination)
+def get_food_at_pos(hungry_org, position):
+    if hungry_org.features['Hostile']:
+        for org in org_compendium:
+            if position == org.position and hungry_org != org:
+                return org
+    else:
+        for food in food_compendium:
+            if position == food.position:
+                return food
 
 
 class Organism:
 
-    def __init__(self, pos, genome=create_first_genome()):
+    def __init__(self, env, pos, genome=create_first_genome()):
         self.position = pos
         self.genome = genome
-        self.id = len(org_compendium) + 1
+        self.id = len(org_compendium) * 2
         org_compendium.insert(self.id, self)
         self.features = interpret_genome(genome)
-        self.initialize_traits()
+        self.initialize_features()
         self.alive = True
+        env.add_org(self)
 
+    # Organism traits/ inherent properties
     def to_string(self):
-        print('Id: {} Health: {} Position: {} Genome: {}'
-              .format(self.get_id(), self.get_health(), self.get_position(), self.get_genome()))
+        print('Id: {} Health: {} Position: {} Is Alive: {}'
+              .format(self.id, self.features['Health'], self.position, self.alive))
 
-    def initialize_traits(self):
+    def initialize_features(self):
+        self.initialize_behavior()
         self.initialize_health()
         self.initialize_attack()
         self.initialize_speed()
-        self.initialize_behavior()
+        self.features['Age'] = 0
 
     def initialize_speed(self):
         if self.features['Agility'] == 'Fast':
@@ -180,17 +211,11 @@ class Organism:
         else:
             self.features['Speed'] = 1
 
-    def get_speed(self):
-        return self.features['Speed']
-
     def initialize_health(self):
         if self.features['Size'] == 'Large':
             self.features['Health'] = random.randint(75, 100)
         else:
             self.features['Health'] = random.randint(30, 55)
-
-    def get_health(self):
-        return self.features['Health']
 
     def initialize_attack(self):
         if self.features['Size'] == 'Large':
@@ -198,116 +223,118 @@ class Organism:
         else:
             self.features['Attack'] = random.randint(15, 25)
 
-    def get_attack(self):
-        return self.features['Attack']
-
     def initialize_behavior(self):
         if self.features['Behavior'] == 'Hostile':
             self.features['Hostile'] = True
         else:
             self.features['Hostile'] = False
 
-    def is_hostile(self):
-        return self.features['Hostile']
-
-    def is_alive(self):
-        return self.alive
-
-    def death(self):
-        self.alive = False
-        self.to_string()
-        print('Alas! Organism ' + str(self.get_id()) + ' has perished!')
-        org_compendium.remove(self)
-
-    def get_position(self):
-        return self.position
-
     def set_position(self, new_position):
         self.position = new_position
-
-    def move(self, env, direction=1):
-        movement = self.get_position() + direction * self.get_speed()
-        if self.get_position() + movement > env.get_len() or self.get_position() + movement < env.get_len():
-            return
-        if not is_occupied(movement):
-            self.set_position(movement)
-            env.modify_pos(self, movement)
-        else:
-            self.set_position(movement)
-            env.modify_pos(self, movement)
-            consume(self, get_at_pos(movement))
-
-    def get_genome(self):
-        return self.genome
-
-    def get_id(self):
-        return self.id
 
     def set_health(self, health_change):
         self.features['Health'] += health_change
 
+    def set_age(self, age_change):
+        self.features['Age'] += age_change
+
+    def attrition(self, env):
+        if self.features['Age'] > 50:
+            self.set_health(-10)
+        elif self.features['Age'] > 25:
+            self.set_health(-5)
+        elif self.features['Age'] > 10:
+            self.set_health(-2)
+        else:
+            self.set_health(-1)
+        if self.features['Health'] <= 0:
+            print(str(self.id) + ' starved')
+            env.remove_org(self)
+
+    # Organism move and fighting functions
+    def move(self, env):
+        start = self.position
+        coin_flip = random.randint(0, 1)
+        if coin_flip == 1:
+            direction = 1
+        else:
+            direction = -1
+        if len(env.display[start]) >= 2:
+            consume(env, self, get_food_at_pos(self, start))
+            if not self.alive:
+                env.remove_org(self)
+                return
+            return
+        destination = self.position + direction * self.features['Speed']
+        if destination >= env.length or destination < 0:
+            return
+        elif is_occupied(destination):
+            consume(env, self, get_at_pos(destination))
+            if self.alive:
+                env.modify_pos(self, start, destination)
+            else:
+                env.remove_org(self)
+        else:
+            env.modify_pos(self, start, destination)
+
     def hit(self, target):
-        target.set_health(-1 * self.get_attack())
-        if target.get_health() <= 0:
-            target.death()
-
-
-# insert 'grow' food function here
+        target.set_health(-1 * self.features['Attack'])
+        if target.features['Health'] <= 0:
+            print(str(target.id) + ' was killed')
+            target.alive = False
 
 
 class Food:
 
-    def __init__(self, food_pos):
+    def __init__(self, env, food_pos):
         self.position = food_pos
-        self.id = len(food_compendium)
+        self.id = len(food_compendium) * 2 + 1
         food_compendium.insert(self.id, self)
-        self.value = random.randint(10, 15) * random.randint(-1, 1)
+        self.value = random.randint(10, 15)
+        env.add_food(self)
+        self.features = {'Hostile': False, 'Age': 0}
+
+    def get_older(self):
+        self.features['Age'] += 1
 
     def to_string(self):
-        print('Id: {} Food Value: {} Position: {}\n'
-              .format(self.get_id(), self.get_value(), self.get_position()))
-
-    def get_position(self):
-        return self.position
-
-    def get_id(self):
-        return self.id
-
-    def get_value(self):
-        return self.value
-
-    def consumed(self):
-        food_compendium.remove(self)
+        print('Id: {} Food Value: {} Position: {}'
+              .format(self.id, self.value, self.position))
 
 
 class Environment:
 
     def __init__(self, env_len):
         self.length = env_len
-        self.display = [[] for n in range(env_len)]
+        self.display = [[] for _ in range(env_len)]
 
-    def get_len(self):
-        return self.length
+    def add_food(self, food):
+        self.display[food.position].append(food.id)
 
-    def add_obj(self, obj):
-        self.get_display()[obj.get_position()].append(obj.get_id())
+    def remove_food(self, food):
+        self.display[food.position].remove(food.id)
+        food_compendium.remove(food)
 
-    def modify_pos(self, obj, destination):
-        self.get_display()[destination].append(obj.get_id())
-        for org in org_compendium:
-            if org.get_position() != destination and org.get_id() == obj.get_id():
-                del org
-                break
+    def add_org(self, org):
+        self.display[org.position].append(org.id)
 
-    def get_display(self):
-        return self.display
+    def remove_org(self, org):
+        self.display[org.position].remove(org.id)
+        org_compendium.remove(org)
 
-    def get_env(self):
+    def modify_pos(self, org, initial_pos, final_pos):
+        org.set_position(final_pos)
+        self.display[final_pos].append(org.id)
+        self.display[initial_pos].remove(org.id)
+
+    def print_env(self):
         print(self.display)
 
-    def populate(self):
-        for org in org_compendium:
-            self.add_obj(org)
-        for food in food_compendium:
-            self.add_obj(food)
-        self.get_env()
+    def food_grow(self):
+        species = len(org_compendium)
+        if species == 0:
+            for i in range(int(self.length / 4)):
+                Food(self, random.randint(0, self.length - 1))
+        else:
+            for i in range(int(self.length / random.randint(5, 10))):
+                Food(self, random.randint(0, self.length - 1))
